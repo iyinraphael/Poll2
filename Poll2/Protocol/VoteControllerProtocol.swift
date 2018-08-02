fb9f78df6f7916465d2d0f6c9f7f8fc92d718b0d
Iyin Raphael <iyinraphael@gmail.com>
2018-08-02 14:22:09 -0700

    added code for tabBarController

---
+++ Poll2/Protocol/VoteControllerProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  VoteControllerProtocol.swift
+//  Poll2
+//
+//  Created by Iyin Raphael on 8/2/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import Foundation
+
+protocol VoteControllerProtocol: class  {
+    var voteController: VoteController? {get set}
+}
